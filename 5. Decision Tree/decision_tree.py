43ee809266bd3a9aff4db20a9274734f79f8f39e
fuxingyu <fuxy28@mail2.sysu.edu.cn>
2018-10-27 22:31:25 +0800

    update

--- 5. Decision Tree/decision_tree.py
+++ 5. Decision Tree/decision_tree.py
@@ -8,9 +8,7 @@ import matplotlib.pyplot as plt
 
 
 class Node:
-    def __init__(self, input_dim, class_num):
-        self._input_dim = input_dim
-        self._class_num = class_num
+    def __init__(self):
         self.split_index = None
         self.split_value = None
         self.leaf_node = None
@@ -29,10 +27,10 @@ class DecisionTree:
         self._minimal_samples = minimal_samples
 
         # Define the tree root
-        self._root = Node(input_dim, class_num)
+        self._root = Node()
 
     def train(self, x, y):
-        pass
+        self._build_tree(self._root, x, y)
 
     def predict(self, x):
         pass
@@ -40,8 +38,46 @@ class DecisionTree:
     def evaluate(self, x, y):
         pass
 
-    def _gini(self):
+    def _built_tree(self, node, x, y):
+        node_left, x_left, y_left, node_right, x_right, y_right = self._split(node, x, y)
+        self._built_tree(node_left, x_left, y_left)
+        self._built_tree(node_right, x_right, y_right)
+
+    def _split(self, node, x, y):
+
+        best_value = 10000000000000
+        best_index = None
+        best_x_left = None
+        best_y_left = None
+        best_x_right = None
+        best_y_right = None
+
+        for i in range(self._input_dim):
+            for sample in x:
+
+                if self._split_criterion == 'gini':
+                    value, x_left, y_left, x_right, y_right = self._gini(i, sample[i], x, y)
+                elif self._split_criterion == 'cross_entropy':
+                    value, x_left, y_left, x_right, y_right = self._cross_entropy(i, sample[i], x, y)
+                else:
+                    raise ValueError('self._split_criterion cannot be ' + self._split_criterion)
+
+                if value < best_value:
+                    best_value = value
+                    best_index = i
+                    best_x_left = x_left
+                    best_y_left = y_left
+                    best_x_right = x_right
+                    best_y_right = y_right
+
+        node.split_index = best_index
+        node.split_value = best_value
+        node.leaf_node = Node()
+        node.right_node = Node()
+        return node.leaf_node, best_x_left, best_y_left, node.right_node, best_x_right, best_y_right
+
+    def _gini(self, index, value, x, y):
         pass
 
-    def _cross_entropy(self):
+    def _cross_entropy(self, index, value, x, y):
         pass
