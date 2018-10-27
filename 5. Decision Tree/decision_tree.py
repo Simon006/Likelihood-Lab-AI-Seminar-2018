6e570f93eb1b0f0bbc24e951c652a4be7dbf910b
fuxingyu <fuxy28@mail2.sysu.edu.cn>
2018-10-28 00:07:17 +0800

    update

--- 5. Decision Tree/decision_tree.py
+++ 5. Decision Tree/decision_tree.py
@@ -6,6 +6,7 @@ import numpy as np
 from collections import Counter
 from sklearn.datasets import load_iris
 # import matplotlib.pyplot as plt
+import random as rd
 
 
 class Node:
@@ -14,7 +15,7 @@ class Node:
         self.split_value = None
         self.leaf_node = None
         self.right_node = None
-        self.is_terminal = None
+        self.is_terminal = False
         self.category = None
         self.depth = 0
 
@@ -71,13 +72,24 @@ class DecisionTree:
         condition2 = node.depth < self._maximal_depth
 
         if condition1 and condition2:
-            node_left, x_left, y_left, node_right, x_right, y_right = self._split(node, x, y)
-            self._build_tree(node_left, x_left, y_left)
-            self._build_tree(node_right, x_right, y_right)
+            x_left, y_left, x_right, y_right = self._split(node, x, y)
+            if len(x_left) == 0 or len(x_right) == 0:
+                node.is_terminal = True
+                node.category = max(list(y), key=list(y).count)
+            else:
+                node_left = Node()
+                node_left.depth = node.depth + 1
+                node.leaf_node = self._build_tree(node_left, x_left, y_left)
+                node_right = Node()
+                node_right.depth = node.depth + 1
+                node.right_node = self._build_tree(node_right, x_right, y_right)
         else:
             node.is_terminal = True
             node.category = max(list(y), key=list(y).count)
-            return 0
+            print(node.category)
+            return node
+
+        return node
 
     def _split(self, node, x, y):
 
@@ -103,11 +115,7 @@ class DecisionTree:
 
         node.split_index = best_index
         node.split_value = best_value
-        node.leaf_node = Node()
-        node.right_node = Node()
-        node.leaf_node.depth = node.depth + 1
-        node.right_node.depth = node.depth + 1
-        return node.leaf_node, best_x_left, best_y_left, node.right_node, best_x_right, best_y_right
+        return best_x_left, best_y_left, best_x_right, best_y_right
 
     def _gini(self, index, value, x, y):
         left_x_list = []
@@ -128,7 +136,7 @@ class DecisionTree:
         left_y_stat_dict = Counter(left_y_list)
         gini_value_left = 1 - sum([(left_y_stat_dict[key]/len(left_y_list))**2 for key in left_y_stat_dict])
         right_y_stat_dict = Counter(right_y_list)
-        gini_value_right = 1 - sum([(right_y_stat_dict[key]/len(right_y_list))**2 for key in left_y_stat_dict])
+        gini_value_right = 1 - sum([(right_y_stat_dict[key]/len(right_y_list))**2 for key in right_y_stat_dict])
         gini_value = gini_value_left + gini_value_right
 
         return gini_value, np.array(left_x_list), np.array(left_y_list), np.array(right_x_list), np.array(right_y_list)
@@ -139,12 +147,19 @@ if __name__ == '__main__':
     """
     still testing
     """
+
     iris = load_iris()
 
     iris_x = iris['data']
     iris_y = iris['target']
 
-    dt = DecisionTree(4, 3, 100, 1)
+    # Shuffle the data
+    random_idx = rd.sample([i for i in range(len(iris_x))], len(iris_x))
+    iris_x = iris_x[random_idx]
+    iris_y = iris_y[random_idx]
+
+    dt = DecisionTree(4, 3, 10000000000, 2)
     dt.train(iris_x, iris_y)
     acc = dt.evaluate(iris_x, iris_y)
     print(acc)
+    print(dt.predict(iris_x))
