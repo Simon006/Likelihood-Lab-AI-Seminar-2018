78f5bffb17702d8aa1cab9f263b14dc05908f7e6
fuxingyu <fuxy28@mail2.sysu.edu.cn>
2018-11-07 20:04:30 +0800

    update

--- 5. Decision Tree/decision_tree.py
+++ 5. Decision Tree/decision_tree.py
@@ -7,6 +7,7 @@ XingYu
 import numpy as np
 import random as rd
 import matplotlib.pyplot as plt
+from math import log
 from collections import Counter
 from sklearn.datasets import load_wine
 
@@ -26,12 +27,13 @@ class Node:
 
 
 class DecisionTree:
-    def __init__(self, input_dim, class_num, maximal_depth, minimal_samples):
+    def __init__(self, input_dim, class_num, maximal_depth=1000, minimal_samples=1, criterion='gini'):
         # basic classifier information
         self._input_dim = input_dim
         self._class_num = class_num
         self._maximal_depth = maximal_depth
         self._minimal_samples = minimal_samples
+        self._criterion = criterion
 
         # Define the tree root
         self._root = Node()
@@ -99,7 +101,7 @@ class DecisionTree:
             return 0
 
     def _split(self, x, y):
-        best_gini = 100000000
+        best_performance = 100000000
         best_split_index = None
         best_split_value = None
         best_x_left = None
@@ -109,10 +111,15 @@ class DecisionTree:
 
         for i in range(self._input_dim):
             for sample in x:
-                gini, x_left, y_left, x_right, y_right = self._gini(i, sample[i], x, y)
+                if self._criterion == 'gini':
+                    classification_performance, x_left, y_left, x_right, y_right = self._gini(i, sample[i], x, y)
+                elif self._criterion == 'entropy':
+                    classification_performance, x_left, y_left, x_right, y_right = self._entropy(i, sample[i], x, y)
+                else:
+                    raise ValueError('self._criterion cannot be ' + self._criterion)
 
-                if gini < best_gini:
-                    best_gini = gini
+                if classification_performance < best_performance:
+                    best_performance = classification_performance
                     best_split_index = i
                     best_split_value = sample[i]
                     best_x_left = x_left
@@ -122,29 +129,89 @@ class DecisionTree:
 
         return best_split_index, best_split_value, best_x_left, best_y_left, best_x_right, best_y_right
 
-    def _gini(self, index, value, x, y):
+    def _gini(self, split_index, split_value, x, y):
+        if len(x[0]) != self._input_dim:
+            raise ValueError('input dimension does not match.')
+
+        # divide the training data into two groups by index and value
         left_x_list = []
         left_y_list = []
         right_x_list = []
         right_y_list = []
-
         for i in range(len(x)):
             sample = x[i]
             label = y[i]
-            if sample[index] < value:
+            if sample[split_index] < split_value:
                 left_x_list.append(sample)
                 left_y_list.append(label)
             else:
                 right_x_list.append(sample)
                 right_y_list.append(label)
 
+        # calculate gini of the left node
+        # the key of the dictionary(left_y_stat_dict) is class label;
+        # the value of dictionary is the corresponding frequency;
         left_y_stat_dict = Counter(left_y_list)
-        gini_value_left = sum([(left_y_stat_dict[key]/len(left_y_list))*(1-(left_y_stat_dict[key]/len(left_y_list))) for key in left_y_stat_dict])
+        gini_value_left = sum([(left_y_stat_dict[key]/len(left_y_list))*(1-(left_y_stat_dict[key]/len(left_y_list)))
+                               for key in left_y_stat_dict])
+        left_prob = len(left_y_list) / (len(left_y_list) + len(right_y_list))
 
+        # calculate gini of the right node
         right_y_stat_dict = Counter(right_y_list)
-        gini_value_right = sum([(right_y_stat_dict[key]/len(right_y_list))*(1-(right_y_stat_dict[key]/len(right_y_list))) for key in right_y_stat_dict])
+        gini_value_right = sum([(right_y_stat_dict[key]/len(right_y_list))*(1-(right_y_stat_dict[key]/len(right_y_list)))
+                                for key in right_y_stat_dict])
+        right_prob = len(right_y_list) / (len(left_y_list) + len(right_y_list))
 
-        gini_value = gini_value_left + gini_value_right
+        # calculate comprehensive gini
+        gini_value = gini_value_left * left_prob + gini_value_right * right_prob
+
+        return gini_value, np.array(left_x_list), np.array(left_y_list), np.array(right_x_list), np.array(right_y_list)
+
+    def _entropy(self, split_index, split_value, x, y):
+        if len(x[0]) != self._input_dim:
+            raise ValueError('input dimension does not match.')
+
+        # divide the training data into two groups by index and value
+        left_x_list = []
+        left_y_list = []
+        right_x_list = []
+        right_y_list = []
+        for i in range(len(x)):
+            sample = x[i]
+            label = y[i]
+            if sample[split_index] < split_value:
+                left_x_list.append(sample)
+                left_y_list.append(label)
+            else:
+                right_x_list.append(sample)
+                right_y_list.append(label)
+
+        # calculate gini of the left node
+        # the key of the dictionary(left_y_stat_dict) is class label;
+        # the value of dictionary is the corresponding frequency;
+        left_y_stat_dict = Counter(left_y_list)
+        entropy_value_left = 0
+        for key in left_y_stat_dict:
+            p = left_y_stat_dict[key] / len(left_y_list)
+            if p != 0:
+                entropy_value_left -= p * log(p)
+            else:
+                entropy_value_left -= 0
+        left_prob = len(left_y_list) / (len(left_y_list) + len(right_y_list))
+
+        # calculate gini of the right node
+        right_y_stat_dict = Counter(right_y_list)
+        entropy_value_right = 0
+        for key in right_y_stat_dict:
+            p = right_y_stat_dict[key] / len(right_y_list)
+            if p != 0:
+                entropy_value_right -= p * log(p)
+            else:
+                entropy_value_right -= 0
+        right_prob = len(right_y_list) / (len(left_y_list) + len(right_y_list))
+
+        # calculate comprehensive gini
+        gini_value = entropy_value_left * left_prob + entropy_value_right * right_prob
 
         return gini_value, np.array(left_x_list), np.array(left_y_list), np.array(right_x_list), np.array(right_y_list)
 
@@ -174,7 +241,7 @@ if __name__ == '__main__':
     acc_list = []
     depth_list = [(i+1) for i in range(20)]
     for depth in depth_list:
-        dt = DecisionTree(input_dim=13, class_num=3, maximal_depth=depth, minimal_samples=1)
+        dt = DecisionTree(input_dim=13, class_num=3, maximal_depth=depth, minimal_samples=1, criterion='entropy')
         dt.train(train_x, train_y)
         acc = dt.evaluate(test_x, test_y)
         acc_list.append(acc)
@@ -188,7 +255,7 @@ if __name__ == '__main__':
     acc_list = []
     sample_num_list = [(i+1) for i in range(30)]
     for sample_num in sample_num_list:
-        dt = DecisionTree(input_dim=13, class_num=3, maximal_depth=1000, minimal_samples=sample_num)
+        dt = DecisionTree(input_dim=13, class_num=3, maximal_depth=100, minimal_samples=sample_num, criterion='entropy')
         dt.train(train_x, train_y)
         acc = dt.evaluate(test_x, test_y)
         acc_list.append(acc)
