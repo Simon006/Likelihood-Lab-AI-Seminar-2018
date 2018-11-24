770725977f518428ae39db0c966d266e002d2247
fuxingyu <fuxy28@mail2.sysu.edu.cn>
2018-11-24 11:47:57 +0800

    update

--- 6. Random Forest/random_forest.py
+++ 6. Random Forest/random_forest.py
@@ -3,8 +3,15 @@ from decision_tree import DecisionTree
 
 
 class RandomForest:
-    def __init__(self, class_num, maximal_depth=1000, minimal_samples=1, criterion='gini'):
-        pass
+    def __init__(self, input_dim, tree_num, maximal_depth=1000, minimal_samples=1, criterion='gini'):
+        # basic classifier information
+        self._input_dim = input_dim
+        self._tree_num = tree_num
+        self._maximal_depth = maximal_depth
+        self._minimal_samples = minimal_samples
+        self._criterion = criterion
+
+        #
 
     def train(self):
         pass
