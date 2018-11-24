bc16b98aec87143623801fcb21e50f1d0af4a1b2
fuxingyu <fuxy28@mail2.sysu.edu.cn>
2018-11-24 21:03:47 +0800

    update

--- 7. Boosting Tree/boosting_tree.py
+++ 7. Boosting Tree/boosting_tree.py
@@ -96,3 +96,33 @@ class BoostingTree:
                 continue
         accuracy = correct_num / len(y)
         return accuracy
+
+
+if __name__ == '__main__':
+    # load wine data
+    # each sample has 13 features and 3 possible classes
+    wine = load_wine()
+    wine_x = wine['data']
+    wine_y = wine['target']
+
+    # shuffle the data randomly
+    random_idx = rd.sample([i for i in range(len(wine_x))], len(wine_x))
+    wine_x = wine_x[random_idx]
+    wine_y = wine_y[random_idx]
+
+    # split the data into training data set and testing data set
+    train_rate = 0.4
+    train_num = int(train_rate*len(wine_x))
+    train_x = wine_x[:train_num]
+    train_y = wine_y[:train_num]
+    test_x = wine_x[train_num:]
+    test_y = wine_y[train_num:]
+
+    # compare the performances of random forest and decision tree
+    bt = BoostingTree(input_dim=len(train_x[0]), tree_num=100, maximal_depth=2, minimal_samples=5, criterion='gini')
+    dt = DecisionTree(input_dim=len(train_x[0]), maximal_depth=10, minimal_samples=5, criterion='gini')
+    bt.train(train_x, train_y)
+    dt.train(train_x, train_y)
+    print('Boosting Tree Accuracy: ' + str(bt.evaluate(test_x, test_y)))
+    print('Decision Tree Accuracy: ' + str(dt.evaluate(test_x, test_y)))
+
