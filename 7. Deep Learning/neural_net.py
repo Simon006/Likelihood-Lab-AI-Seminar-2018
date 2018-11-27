06d9746418bc76f2598e2a6d347550e448dfc791
fuxingyu <fuxy28@mail2.sysu.edu.cn>
2018-11-28 02:01:08 +0800

    update

--- 7. Deep Learning/neural_net.py
+++ 7. Deep Learning/neural_net.py
@@ -1,35 +1,39 @@
 import numpy as np
 from math import exp
+import random as rd
+from sklearn.datasets import load_wine
 
 
 class NeuralNetwork:
-    def __init__(self, input_dim, output_dim, neuron_num_list, learning_rate):
+    def __init__(self, input_dim, output_dim, neuron_num_list, learning_rate, epoch):
         # basic neural net information
         self._input_dim = input_dim
         self._output_dim = output_dim
         self._neuron_num_list = neuron_num_list
         self._learning_rate = learning_rate
+        self._epoch = epoch
 
         # construct network
         self._network = self._initialize_network()
 
     def train(self,x ,y):
-        # minimize the loss function in each training sample through SGD
-        for i in range(len(x)):
-            input_output_record = []
-            tensor = x[i]
-            # forward propagation
-            for forward_step in range(len(self._network)):
-                input_output_record[forward_step] = dict()
-                input_output_record[forward_step]['input'] = tensor
-                tensor = np.dot(self._network[i]['weight'], tensor) + self._network[i]['bias']
-                tensor = _sigmoid_activation(tensor)
-                input_output_record[forward_step]['output'] = tensor
+        for e in range(self._epoch):
+            # minimize the loss function in each training sample through SGD
+            for i in range(len(x)):
+                input_output_record = []
+                tensor = x[i]
+                # forward propagation
+                for forward_step in range(len(self._network)):
+                    input_output_record[forward_step] = dict()
+                    input_output_record[forward_step]['input'] = tensor
+                    tensor = np.dot(self._network[i]['weight'], tensor) + self._network[i]['bias']
+                    tensor = _sigmoid_activation(tensor)
+                    input_output_record[forward_step]['output'] = tensor
 
-            # backward propagation
-            for backward_step in reversed(range(len(self._network)-1)):
-                self._network['weight'][backward_step] = self._network['weight'][backward_step] + self._learning_rate
-                self._network['bias'][backward_step] = self._network['bias'][backward_step] + self._learning_rate
+                # backward propagation
+                for backward_step in reversed(range(len(self._network)-1)):
+                    self._network['weight'][backward_step] = self._network['weight'][backward_step] + self._learning_rate
+                    self._network['bias'][backward_step] = self._network['bias'][backward_step] + self._learning_rate
 
     def predict(self, x):  # forward propagation
         y_predict = np.zeros((len(x), self._output_dim))
@@ -85,10 +89,43 @@ def _derivative_sigmoid_activation(vector):
     return _sigmoid_activation(vector) - _sigmoid_activation(vector) * _sigmoid_activation(vector)
 
 
+def num_2_one_hot(y):
+    one_hot_array = np.zeros((len(y), len(set(y))))
+    response_2_category = dict()
+    for index, response in enumerate(set(y)):
+        response_2_category[response] = index
+    for index, response in enumerate(y):
+        one_hot_array[index][response_2_category[response]] = 1
+    return one_hot_array
+
+
 if __name__ == '__main__':
-    # forward propagation test
-    nn = NeuralNetwork(5, 2, [3, 2, 2])
-    vector = np.array([[1, 0, 1, 0, 1]])
-    answer = np.array([[0.5, 0.5]])
-    print(nn.predict(vector))
-    print(nn.evaluate(vector, answer))
+    # load wine data
+    # each sample has 13 features and 3 possible classes
+    wine = load_wine()
+    wine_x = wine['data']
+    wine_y = wine['target']
+
+    # shuffle the data randomly
+    random_idx = rd.sample([i for i in range(len(wine_x))], len(wine_x))
+    wine_x = wine_x[random_idx]
+    wine_y = wine_y[random_idx]
+
+    # convert label into one hot format
+    wine_y = num_2_one_hot(wine_y)
+
+    # split the data into training data set and testing data set
+    train_rate = 0.7
+    train_num = int(train_rate*len(wine_x))
+    train_x = wine_x[:train_num]
+    train_y = wine_y[:train_num]
+    test_x = wine_x[train_num:]
+    test_y = wine_y[train_num:]
+
+    # train neural net to predict
+    dnn = NeuralNetwork(input_dim=len(train_x[0]), output_dim=len(train_y[0]),
+                        neuron_num_list=[15, 10, 5, len(train_y[0])],
+                        learning_rate=0.1, epoch=20)
+    dnn.train(x=train_x, y=train_y)
+    mse = dnn.evaluate(x=test_x, y=test_y)
+    print('Test MSE: ' + str(mse))
