ad7bb0b65efa78ba575f540c84994c8740bb160f
fuxingyu <fuxy28@mail2.sysu.edu.cn>
2018-12-02 13:32:24 +0800

    update

--- 8. Deep Q Learning/env.py
+++ 8. Deep Q Learning/env.py
@@ -1,13 +1,50 @@
 import gym
+from dqn import DeepQNet
 
 
-class Env:
-    def __init__(self):
-        pass
+class SpaceInvadersEnv:
+    def __init__(self, agent, epoch):
+        # construct gym space invaders environment
+        self._env = gym.make('SpaceInvaders-v0')
+        self._env = self._env.unwrapped
+
+        # define the reinforcement learning agent
+        self._agent = agent
+
+        # number of games to play
+        self._epoch = epoch
+
+    def run(self):
+        for e in range(self._epoch):
+            # receive initial observation
+            observation_current = self._env.reset()
+
+            while True:
+                # render the game to screen
+                self._env.render()
+
+                # reinforcement learning agent choose which action to take
+                action = self._agent.choose_action(observation_current)
+
+                # environment receives the action the agent took
+                observation_next, reward, done, info = self._env.step(action)
+
+                # store the data for training
+                self._agent.store_train_data(observation_current, action, reward, observation_next)
+
+                # train the agent when data is enough
+                if self._agent.is_enough_data():
+                    self._agent.train()
+
+                # if the game is finished, we reset the game to restart.
+                # if the game is not finished, we keep on playing in the current game.
+                if done:
+                    break
+                else:
+                    observation_current = observation_next
 
 
 if __name__ == '__main__':
-    env = gym.make('SpaceInvaders-v0')
-    env.reset()
-    env.render()
-    env.close()
+    dqn = DeepQNet()
+    env = SpaceInvadersEnv(agent=dqn, epoch=1000)
+    env.run()
