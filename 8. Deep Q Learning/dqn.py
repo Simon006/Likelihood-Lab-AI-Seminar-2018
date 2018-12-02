e6b86bfcccaa721b784d532f60f78cb856dbdab4
fuxingyu <fuxy28@mail2.sysu.edu.cn>
2018-12-02 15:33:00 +0800

    update

--- 8. Deep Q Learning/dqn.py
+++ 8. Deep Q Learning/dqn.py
@@ -1,21 +1,20 @@
 import numpy as np
 from keras.engine.topology import Input
 from keras.engine.training import Model
-from keras.layers import add
-from keras.layers.convolutional import Conv2D
-from keras.layers.core import Activation, Dense, Flatten
+from keras.layers.core import Activation, Dense
 from keras.layers.normalization import BatchNormalization
 from keras.regularizers import l2
 from keras.optimizers import SGD
 
 
 class DeepQNet:
-    def __init__(self, n_actions, n_features, learning_rate, l2_coefficient, e_greedy, memory_size):
+    def __init__(self, n_actions, n_features, learning_rate, momentum, l2_penalty, e_greedy, memory_size):
         # basic agent information
         self._n_actions = n_actions
         self._n_features = n_features
         self._learning_rate = learning_rate
-        self._l2_coefficient = l2_coefficient
+        self._momentum = momentum
+        self._l2_penalty = l2_penalty
         self._e_greedy = e_greedy
         self._memory_size = memory_size
 
@@ -35,7 +34,27 @@ class DeepQNet:
         pass
 
     def _build_network(self):
+        # define the input format
         init_x = Input((1, 1, self._n_features))
-        x = init_x
 
-        x = Dense(15, kernel_regularizer=l2(self._l2_coefficient))(x)
+        # Multiple Dense Layers
+        x = Dense(15, kernel_regularizer=l2(self._l2_penalty))(init_x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+
+        x = Dense(10, kernel_regularizer=l2(self._l2_penalty))(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+
+        x = Dense(self._n_actions, kernel_regularizer=l2(self._l2_penalty))(x)
+        x = BatchNormalization()(x)
+        x = Activation('softmax')(x)
+
+        # define the network
+        net = Model(inputs=init_x, outputs=x)
+
+        # define the loss function
+        opt = SGD(lr=self._learning_rate, momentum=self._momentum, nesterov=True)
+        net.compile(optimizer=opt, loss='mean_squared_error')
+
+        return net
